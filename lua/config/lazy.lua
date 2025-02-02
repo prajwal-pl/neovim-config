845a2fdd6f3d91f8da3a7e68c8c422ac6a6d9907
Prajwal-PL <prajwalpl096@gmail.com>
2025-02-02 10:12:22 +0530

    Configuring dashboard and removing hererocks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plegins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
--- lua/plugins/hererocks.lua
+++ lua/plugins/hererocks.lua
@@ -1,3 +1,3 @@
 return {
-  "",
+  { "hererocks", enabled = false },
 }
